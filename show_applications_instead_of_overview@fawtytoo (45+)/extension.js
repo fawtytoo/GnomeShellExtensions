05414faa033cf1616fdcece52d106f795dc23ede
twelve eighty pix. <71165491+1280px@users.noreply.github.com>
2024-12-29 23:53:55 +0700

    Don't import Config becuase it's not used for now
    (it will probably be needed in the future, which is why I didn't simply remove it altogether)

--- show_applications_instead_of_overview@fawtytoo (45+)/extension.js
+++ show_applications_instead_of_overview@fawtytoo (45+)/extension.js
@@ -1,7 +1,7 @@
 import * as Overview from 'resource:///org/gnome/shell/ui/overview.js';
 
-import * as Config from 'resource:///org/gnome/shell/misc/config.js'; // currently unused, but
-const Version = parseInt(Config.PACKAGE_VERSION.split('.')[0]); // probably will be needed later
+// import * as Config from 'resource:///org/gnome/shell/misc/config.js'; // currently unused, but
+// const Version = parseInt(Config.PACKAGE_VERSION.split('.')[0]); // probably will be needed later
 
 let oldOverview;
 
